c0524c4890a7bd476e29a722cf53d34f96a5ccaa
Evgenii Abramson <5366163-abramsea@users.noreply.gitlab.com>
2020-06-26 13:06:36 +0300

    init

---
+++ js/switcher.js
@@ -0,0 +1,9 @@
+$('.intro__switch-link1').click(() => {
+    $('#mu-so').hide();
+    $('#qb').show();
+})
+
+$('.intro__switch-link2').click(() => {
+    $('#qb').hide();
+    $('#mu-so').show();
+})
