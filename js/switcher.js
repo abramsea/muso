f8fd5eea5ea5f8c9da6a59532cce5e340309ef0d
Evgenii Abramson <5366163-abramsea@users.noreply.gitlab.com>
2020-06-26 17:23:06 +0300

    pre-slider

--- js/switcher.js
+++ js/switcher.js
@@ -27,3 +27,50 @@ $('#dot2').click(() => {
     $('#qb').hide();
     $('#mu-so').show();
 })
+
+
+
+
+$('#muso-black').click( () => {
+    $('#muso-color').attr('src', 'images/black.jpg')
+})
+
+$('#muso-green').click( () => {
+    $('#muso-color').attr('src', 'images/olive.jpg')
+})
+
+$('#muso-red').click( () => {
+    $('#muso-color').attr('src', 'images/red.jpg')
+})
+
+$('#muso-blue').click( () => {
+    $('#muso-color').attr('src', 'images/blue.jpg')
+})
+
+$('#muso-none').click( () => {
+    $('#muso-color').attr('src', 'images/no-grill.jpg')
+})
+
+
+
+
+$('#qb-black').click( () => {
+    $('#qb-color').attr('src', 'images/qb-black.jpg')
+})
+
+$('#qb-green').click( () => {
+    $('#qb-color').attr('src', 'images/qb-olive.jpg')
+})
+
+$('#qb-red').click( () => {
+    $('#qb-color').attr('src', 'images/qb-red.jpg')
+})
+
+$('#qb-blue').click( () => {
+    $('#qb-color').attr('src', 'images/qb-blue.jpg')
+})
+
+$('#qb-none').click( () => {
+    $('#qb-color').attr('src', 'images/qb-no-grill.jpg')
+})
+
