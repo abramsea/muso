bc5baaea0316216d8105a083b95e77002cf85f15
Evgenii Abramson <5366163-abramsea@users.noreply.gitlab.com>
2020-06-26 15:58:30 +0300

    half-done

--- js/switcher.js
+++ js/switcher.js
@@ -7,3 +7,23 @@ $('.intro__switch-link2').click(() => {
     $('#qb').hide();
     $('#mu-so').show();
 })
+
+$('.intro__switch-link3').click(() => {
+    $('#mu-so').hide();
+    $('#qb').show();
+})
+
+$('.intro__switch-link4').click(() => {
+    $('#qb').hide();
+    $('#mu-so').show();
+})
+
+$('#dot1').click(() => {
+    $('#mu-so').hide();
+    $('#qb').show();
+})
+
+$('#dot2').click(() => {
+    $('#qb').hide();
+    $('#mu-so').show();
+})
